9a8c81067f11ef83c6f820daeaf00922c5ecab59
PedrooMota <pedrohmota2005@gmail.com>
2023-10-24 19:50:44 -0300

    final commit

--- components/pages/dashboard/CompressorData.tsx
+++ components/pages/dashboard/CompressorData.tsx
@@ -1,131 +1,71 @@
-import LineChart from "./LineChart";
-import AreaChart from "./AreaChart";
+'use client'
+
+import TableDashboard from "../../materialUi/Table"
+import { URL_API } from '../../../utils/constants'; 
+import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useEffect, useState } from "react";
-import { URL_API } from '../../../utils/constants';
-
-
-interface Logs {
-  dates: string[],
-  runtimes: number[]
-}
 
 interface UserCompressor {
-  idCompressor: string
+  idCompressor: number
 }
 
-export default function GraficosDashboard( { idCompressor }: UserCompressor) {
+export default function DadosCompressor({ idCompressor }:UserCompressor ) {
 
-  const { data: session } = useSession()
 
-  const [daily, setDaily] = useState<Logs>()
-  const [accumulated, setAccumulated] = useState<Logs>()
-
-  // ----------------------------------------------------
-
-  function formatarDataParaString(data: Date): string {
-    const dia = String(data.getDate()).padStart(2, '0');
-    const mes = String(data.getMonth() + 1).padStart(2, '0'); // Lembre-se que os meses começam do zero
-    const ano = String(data.getFullYear()).slice(-2);
-
-    return `${dia}/${mes}/${ano}`
-  }
-
-  // ----------------------------------------------------
-
-  function formatarStringParaMinutos(dados: string): number {
-    const hora = Number(dados.substring(0, 2))
-    const minutos = Number(dados.substring(3, 5))
-    const total = (hora * 60) + (minutos)
-
-    return total
-  }
-
-  // ----------------------------------------------------
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  function analisarDados(dados: any[]): Logs {
-    const arrayDatas: string[] = []
-    const arrayRuntimes: number[] = []
-
-    dados.map((item: any) => {
-      const date = new Date(item.log_date)
-      const runTime = item.total_runtime
-
-      const newRuntime = formatarStringParaMinutos(runTime)
-      arrayRuntimes.push(newRuntime)
-
-      const newDate = formatarDataParaString(date)
-      arrayDatas.push(newDate)
-    })
-
-    const result: Logs = {
-      dates: arrayDatas,
-      runtimes: arrayRuntimes
-    }
-
-    return result
-  }
+  const [nameCompressor, setNameCompressor] = useState('')
+  const [serialCompressor, setSerialCompressor] = useState(0)
+    
 
   useEffect(() => {
-    
     const fetchData = async () => {
-      
       try {
-        const resp = await fetch(`${URL_API}/compressors/${idCompressor}/logs`, {
-          next: {
-            revalidate: 300,
-          },
-        })
+          const res = await fetch(`${URL_API}/compressors/${idCompressor}`, {
+            next: {
+              revalidate: 300,
+            },
+          })
+          const resp = await res.json()
 
-        if (!resp.ok) {
-          throw new Error('Failed to fetch data')
-        }
-
-        const resposta = await resp.json()
-
-        let daily_runtime_accumulated = resposta.daily_runtime_accumulated
-        daily_runtime_accumulated = analisarDados(daily_runtime_accumulated)
-
-        // ----------------------------------------------
-
-        let daily_runtimes = resposta.daily_runtime
-        daily_runtimes = analisarDados(daily_runtimes)
-
-        setDaily(daily_runtimes)
-        setAccumulated(daily_runtime_accumulated)
-
+          const name = resp.data.name;
+          const serial_number = resp.data.serial_number;
+          
+          setNameCompressor(name)
+          setSerialCompressor(serial_number)
+  
       } catch (error) {
         console.error(error);
       }
     };
-
+  
     fetchData();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [session]);
+  }, [idCompressor]);
 
+  
   return (
     <>
-      <section className="grid gap-20">
-        <div className='md:grid md:grid-cols-12 md:gap-6 items-center'>
-          <div className="max-w-xl md:max-w-none md:w-full mx-auto sm:col-span-2 md:col-span-6 lg:col-span-6 mb-10 bg-white shadow rounded-lg">
-            <div className='md:pr-4 lg:pr-12 xl:pr-16'>
-              <div className="px-6 py-5 font-semibold border-b border-gray-100">Tempo de operação acumulado</div>
-              <div className="p-2 max-w-full mx-auto md:max-w-none h-auto">
-                <AreaChart daily_runtime_accumulated={accumulated?.runtimes} log_date={accumulated?.dates} />
+        <section className="grid gap-20">
+            <div className='md:grid md:grid-cols-12 md:gap-6 items-center'>
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto sm:col-span-2 md:col-span-6 lg:col-span-6 mb-10 bg-white shadow rounded-lg">
+                <div className='md:pr-4 lg:pr-12 xl:pr-16'>
+                  <div className="px-6 py-5 font-semibold border-b border-gray-100">Dados do compressor</div>
+                  <div className="p-2 max-w-full mx-auto md:max-w-none h-auto">
+                    <TableDashboard name={nameCompressor} serial_number={serialCompressor} />
+                  </div>
+                </div>
+              </div>
+              <div className="max-w-xl md:max-w-none md:w-full mx-auto sm:col-span-2 md:col-span-6 lg:col-span-6 mb-10 bg-white shadow rounded-lg">
+                <div className='md:pr-4 lg:pr-12 xl:pr-16'>
+                  <div className="px-6 py-5 font-semibold border-b border-gray-100">Dados do compressor</div>
+                  <div className="p-2 max-w-full mx-auto md:max-w-none h-auto">
+                    teste
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="max-w-xl md:max-w-none md:w-full mx-auto sm:col-span-2 md:col-span-6 lg:col-span-6 mb-10 bg-white shadow rounded-lg">
-            <div className='md:pr-4 lg:pr-12 xl:pr-16'>
-              <div className="px-6 py-5 font-semibold border-b border-gray-100">Tempo de utilização por hora</div>
-              <div className="p-2 max-w-full mx-auto md:max-w-none h-auto">
-                <LineChart daily_runtime={daily?.runtimes} log_date={daily?.dates} />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+    
     </>
   )
 }
+
+
