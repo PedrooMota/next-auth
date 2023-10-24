237607ebf9e74815c234530335d4224450de9327
PedrooMota <pedrohmota2005@gmail.com>
2023-10-24 17:19:50 -0300

    build vercel

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,8 +2,6 @@ import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from 'next-auth/providers/credentials';
 
-export const dynamic = 'force-static'
-
 const authOptions: NextAuthOptions = {
     providers: [
         CredentialsProvider({
